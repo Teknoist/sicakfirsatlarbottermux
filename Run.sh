7bf46745db88042e10604c666468f36ce025cfa8
Teknoist <37031361+Teknoist@users.noreply.github.com>
2020-05-30 16:49:48 +0300

    Update Run.sh
    O

--- Run.sh
+++ Run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-yes | pkg install git python
+yes | pkg install git python clang libxml2 libxslt 
+pip install wheel
+pip install cython
+pip install lxml
 git clone https://github.com/ismkdc/donanimhaber-sicak-firsat-bot
 cd ./donanimhaber-sicak-firsat-bot
 pip install -r requirements.txt
+
